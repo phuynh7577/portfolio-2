c5204db556bd419d2a3d9e6bb27899708e70f7e3
Phi Huynh <phuynh7577@gmail.com>
2020-06-18 18:47:46 -0400

    updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
 import './index';
-import {Route} from 'react-router-dom'
-
-import { spring, AnimatedSwitch } from 'react-router-transition';
 
 import Bio from "./components/Bio"
 import About from "./components/About"
@@ -14,40 +11,6 @@ import Experiences from "./components/Experiences"
 
 class App extends Component {
   render() {
-
-    // we need to map the `scale` prop we define below
-// to the transform style property
-function mapStyles(styles) {
-  return {
-    opacity: styles.opacity,
-    transform: `scale(${styles.scale})`,
-  };
-}
-
-// wrap the `spring` helper to use a bouncy config
-function bounce(val) {
-  return spring(val, {
-    stiffness: 50,
-    damping: 10,
-  });
-}
-
-// child matches will...
-const bounceTransition = {
-  // start in a transparent, upscaled state
-  atEnter: {
-    opacity: 0,
-    scale: 1.2,
-  },
-  // leave in a transparent, downscaled state
-  atLeave: {
-  },
-  // and rest at an opaque, normally-scaled state
-  atActive: {
-    opacity: bounce(1),
-    scale: bounce(1),
-  },
-};
     return (
       <div className="App">
           <Navbar/>
@@ -55,18 +18,6 @@ const bounceTransition = {
           <About/>
           <Projects/>
           <Experiences/>
-            {/* <AnimatedSwitch
-              atEnter={bounceTransition.atEnter}
-              atActive={bounceTransition.atActive}
-              atLeave={bounceTransition.atLeave}
-              mapStyles={mapStyles}
-              className="route-wrapper"
-              >
-                <Route exact path="/" render={() => <Bio/>}/>
-                <Route exact path="/projects" render={() => <Projects/>}/>
-                <Route exact path="/experiences" render={() => <Experiences/>}/>
-                <Route exact path="/skills" render={() => <Skills/>}/>
-            </AnimatedSwitch> */}
           <Footer/>
       </div>
     )
